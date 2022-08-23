cf8a9b5b798e317487a9d1c010500a87f2050ccc
Khwarezmia <krosjean@126.com>
2022-08-23 23:00:54 +0800

    0823night

--- testdate.php
+++ testdate.php
@@ -1,11 +1,3 @@
 <?php
-//$dtz = new DateTimeZone('Asia/Shanghai');
-//$dti = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '1970-01-01 08:00:00', $dtz);
-
-$basetime = strtotime('1970-01-01 08:00:00');
-$intervals = 1660214552501;
-//$newtime = strtotime("+{$intervals} ms", $basetime);
-$newtime = (int) ($basetime + $intervals/1000);
-echo $basetime.PHP_EOL.$intervals.PHP_EOL.date('Y:m:d H:i:s', $newtime).PHP_EOL;
-
-//echo $dti->format('Y-m-d H:i:s').PHP_EOL;
+ echo microtime(true).PHP_EOL;
+ echo time().PHP_EOL;
